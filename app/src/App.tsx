1b1a74a96f55351cfac4835af075710227af0984
Leo <leo@joinvitamin.com>
2022-09-20 01:58:55 +0200

    Separate App to new file

---
+++ app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Equation validator</h1>
+      <p>
+        Is <span className="equation">1 + 2 = 3</span> valid?
+      </p>
+    </>
+  );
+};
+
+export default App;
